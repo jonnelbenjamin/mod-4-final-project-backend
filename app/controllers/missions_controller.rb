3ed4450e9b13805ce002c09a92438e741f90d4bc
github username <github email address>
2019-02-07 21:47:33 -0500

    Controllers updates

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -2,4 +2,8 @@ class MissionsController < ApplicationController
   def index
     render json: Mission.all
   end
+
+  def create
+    Mission.create(params)
+  end
 end
--- app/controllers/spies_controller.rb
+++ app/controllers/spies_controller.rb
@@ -2,4 +2,8 @@ class SpiesController < ApplicationController
   def index
     render json: Spy.all
   end
+
+  def create
+    Spy.create(params)
+  end
 end
